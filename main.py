7d639c61305700207333ef3d636b1177ab8a1a55
fbergh <freekbergh@gmail.com>
2020-10-29 11:42:53 +0100

    Added timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import input_output as io
 import algorithm as a
 from social_graph import SocialGraph
+import time
 
 
 """
@@ -11,10 +12,12 @@ if __name__ == '__main__':
     io.give_credentials()
     n_problems = io.parse_number_of_problems()
     n_correct = 0
+    start_time = time.time()
     for problem in range(n_problems):
         input_parameters = io.get_values_from_input()
         g = SocialGraph(None, input_parameters)
-        is_success = a.zero_point_solution(g)
+        is_success = a.basic_divide_and_conquer(g)
         io.eprint(f"Problem {problem}: {is_success}")
         n_correct += is_success
+    io.eprint(f"Time elapsed: {time.time() - start_time} sec")
     io.eprint(f"Problems correct: {n_correct}/{n_problems}")
