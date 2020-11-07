69703c6ed65beb720509560e2b624cc4bbc3ed92
MaxDriessen <driessenmax@gmail.com>
2020-11-07 22:48:49 +0100

    worked on contact group testing

--- algorithms/contact_group_testing.py
+++ algorithms/contact_group_testing.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 from algorithms import Algorithm
 import input_output as io
@@ -12,37 +13,93 @@ class ContactGroupTesting(Algorithm):
     def run(self, graph):
         # Set global variables
         self.database = {} # database for storing known positives & negatives
-        self.n_found_clusters = 0 # number of found clusters
         self.n_known_positive = 0 # number of known positives
+        self.n_tests = 0 # counter for the number of performed tests
         self.graph = graph # graph on which the algorithm is called
 
         # Compute list of connected components
         self.connected_components = self.get_connected_components()
 
-        # WTH IS GOING ON HERE???
-        for i in self.connected_components:
-            io.send_test(i)
-        for i in self.connected_components:
-            io.eprint(i)
-            io.eprint(io.get_test_result())
-
         # Solve the problem for each component
         self.solve_all_components()
 
-        error # I only want to test a single graph for now, so this is how I make the system crash
+        # If the upper bound was reached, fill in the rest of the database
+        if self._reached_upper_bound():
+            for i in range(self.graph.n_nodes):
+                if i not in self.database:
+                    self.database[i] = False
+
+        # Verify we are done testing and extract the positive nodes 
+        assert len(self.database) == self.graph.n_nodes, "The algorithm has not finished testing"
+        positive_result_idcs = [v for v in self.database if self.database[v]]
+
+        # Send answer and check result
+        io.send_answer(positive_result_idcs)
+        is_success = io.get_problem_result()
+
+        # Return result and number of tests used
+        return is_success, self.n_tests
     
 
     ### SOLVING ALL COMPONENTS ###
 
     def solve_all_components(self):
-        pass #TODO
+        # Get the optimal number of groups for divide-and-conquer
+        p_infection_component = self._calculate_p_infection_component()
+        optimal_group_size = self._get_optimal_group_size(p_infection_component, len(self.connected_components))
+        n_groups = round(self.graph.n_nodes / optimal_group_size) # Approximate n_groups to the closest integer resulting from n_nodes/optimal_group_size
+
+        # Solve components via a divide-and-conquer-like strategy (split into groups, test, recurse if positive)
+        self._solve_all_components_helper(self.connected_components, n_groups)
+
+    def _solve_all_components_helper(self, components, n_groups):
+        # Base case: check every group individually
+        if len(components) < n_groups:
+            for c in components:
+                if not self._reached_upper_bound:
+                    self.solve_component(c)
+        # Recursive case: split components into groups and recurse if these groups test positive
+        else:
+            group_size = len(components)/n_groups
+            split_indices = list(set([math.floor(group_size*i) for i in range(n_groups+1)]))
+
+            for i in range(n_groups):
+                if not self._reached_upper_bound():
+                    start = split_indices[i]
+                    end = split_indices[i+1]
+
+                    union = [node for c in components[start:end] for node in c]
+
+                    result, _, _ = self.test(union)
+                    if result:
+                        self._solve_all_components_helper(components[start:end], n_groups)
+
 
 
     ### SOLVING A SINGLE COMPONENT (WITH AT LEAST 1 POSITIVE NODE) ###
 
     def solve_component(self, component):
-        pass #TODO
-        
+        if len(component) == 1:
+            assert component[0] in self.database, "Lonely component, not stored"
+        # while not self._solved(component):
+        #     sample = self._get_sample(component)
+            
+
+
+    def _solved(self, nodes):
+        for node in nodes:
+            if node not in self.database:
+                return False
+        return True
+
+    def _get_sample(self, component):
+        fraction = 0.1 # Intermediate solution for computing how much to sample
+        sample = []
+        while len(sample) < fraction*len(component):
+            # I did this instead of random.sample with k = ceil(fraction*length(component)) so that we can extend this with non-neighboring vertices
+            sample.extend(random.sample([node for node in component if node not in sample], 1))
+        return sample
+
 
     ### TESTING ###
 
@@ -65,6 +122,7 @@ class ContactGroupTesting(Algorithm):
         if to_test:
             io.send_test(to_test)
             result = io.get_test_result()
+            self.n_tests += 1
 
             # If the result was negative or only a single node was tested, update the database (if one node is tested, that node always solved afterwards)
             if not result or len(to_test) == 1:
@@ -111,3 +169,36 @@ class ContactGroupTesting(Algorithm):
 
         # Return the list of all vertices reachable from the source
         return connected_component
+
+
+    ### UTILITY ###
+
+    # Checking for bounds
+    def _reached_lower_bound(self):
+        return sum(self.database.values()) >= self.graph.lower_bound
+
+    def _reached_upper_bound(self):
+        return sum(self.database.values()) >= self.graph.upper_bound # >= for robustness
+
+    # Calculating optimal group sizes for splitting
+    def _calculate_p_infection_node(self):
+        # Too simple?
+        return (self.graph.lower_bound + self.graph.upper_bound)/(2*self.graph.n_nodes)
+
+    def _calculate_p_infection_component(self):
+        # Best I could think of right now
+        p_infection_node = self._calculate_p_infection_node()
+        avg_component_size = len(self.connected_components)/self.graph.n_nodes
+        p_infection_component = p_infection_node * avg_component_size
+        return p_infection_component
+
+    def _get_optimal_group_size(self, p_infection, upper_bound):
+        # Based on https://blogs.sas.com/content/iml/2020/07/06/pool-testing-covid19.html
+        max_test_reduction, best_k = float('inf'), 0
+        for k in range(1, upper_bound):
+            test_reduction = 1 / k + 1 - (1 - p_infection) ** k
+            if test_reduction < max_test_reduction:
+                max_test_reduction = test_reduction
+                best_k = k
+        return max(best_k, 2)
+        
